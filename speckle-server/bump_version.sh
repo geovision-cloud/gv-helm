b0322a36ce5567838817a3697fb52bf0079be8c1
geovision-cloud <info@geovision.cloud>
2025-08-25 11:36:50 +0200

    Update bump_version.sh

--- speckle-server/bump_version.sh
+++ speckle-server/bump_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yq -i ".docker_image_tag = \"$1\"" speckle-server/values.yaml
-yq -i ".appVersion = \"$1"" speckle-server/Chart.yaml
+yq -i ".appVersion = \"$1\"" speckle-server/Chart.yaml
 yq -i ".version = \"$1\"" speckle-server/Chart.yaml
